747863e5a67ad5e2500adce41737af76c6d3cdbb
unknown <divyesh.patel4878@gmail.com>
2021-03-17 17:02:48 -0400

    SQL

---
+++ FamilyTree/Person.sql
@@ -0,0 +1,11 @@
+create table Person ( personid serial, 
+					firstname text,  
+					fathersname text, 
+					mothersname text, 
+					gender text, 
+					primary key( personid)
+);
+
+
+select * from person p ;
+
